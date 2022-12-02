2e7845478c48acf5ea5dcd241ae230abdbe7bf1a
Elena Lemeskina <gogulinaelena@yandex.ru>
2022-12-02 23:14:22 +0300

    Realization BigDecimal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
@@ -8,10 +9,6 @@ public class Main {
     public static BigDecimal dayMoreNorm = new BigDecimal("8.31");
     public static BigDecimal nightNorm = new BigDecimal("2.20");
     public static BigDecimal nightMoreNorm = new BigDecimal("4.34");
-//    public static double dayNorm = 4.51;
-//    public static double dayMoreNorm = 8.31;
-//    public static double nightNorm = 2.20;
-//    public static double nightMoreNorm = 4.34;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -25,8 +22,8 @@ public class Main {
         System.out.println("Введите текущее показание счетчика за ночь: ");
         int currentNight = scanner.nextInt();
 
-        int consumptionDay = currentDay - previousDay; //потребление дневное
-        int consumptionNight = currentNight - previousNight; //потребление ночное
+        int consumptionDay = currentDay - previousDay; //потребление по тарифу 'День'
+        int consumptionNight = currentNight - previousNight; //потребление по тарифу 'Ночь'
         int sumMonth = consumptionDay + consumptionNight; //сумма потребления за месяц
         System.out.println("Потребление по тарифу 'День' составило: " + consumptionDay +
                 "\nПотребление по тарифу 'Ночь' составило: " + consumptionNight +
@@ -36,35 +33,35 @@ public class Main {
             BigDecimal sumDay = dayNorm.multiply(BigDecimal.valueOf(consumptionDay));
             BigDecimal sumNight = nightNorm.multiply(BigDecimal.valueOf(consumptionNight));
             BigDecimal result = sumDay.add(sumNight);
-//            double sumDay = consumptionDay * dayNorm;
-//            double sumNight = currentNight * nightNorm;
+
             System.out.println("Стоимость по по тарифу 'День' составила: " + sumDay +
                     "\nСтоимость по тарифу 'Ночь' составила: " + sumNight +
                     "\nОбщая стоимость потребления за месяц: " + result);
         } else {
-//            double sumDay = sumDay(consumptionDay, sumMonth);
-//            double sumNight = sumNight(consumptionNight, sumMonth);
 
-//            System.out.println("Стоимость по по тарифу 'День' составила: " + sumDay +
-//                    "\nСтоимость по тарифу 'Ночь' составила: " + sumNight +
-//                    "\nОбщая стоимость потребления за месяц: " + (sumDay + sumNight));
+            BigDecimal consumDay = new BigDecimal(consumptionDay);
+            BigDecimal sumMonD = new BigDecimal(sumMonth);
+            BigDecimal averSumD = consumDay.divide(sumMonD, 2, RoundingMode.CEILING);
+            //точность в две цифры после запятой с округлением в большую сторону
+            BigDecimal averageSumD = averSumD.multiply(BigDecimal.valueOf(norm));
+            BigDecimal sumDayNorm = averageSumD.multiply(dayNorm);
+            BigDecimal sumDayMoreNorm = consumDay.subtract(averageSumD).multiply(dayMoreNorm);
+            BigDecimal sumDay = sumDayNorm.add(sumDayMoreNorm);
+
+            BigDecimal consumNight = new BigDecimal(consumptionNight);
+            BigDecimal sumMonN = new BigDecimal(sumMonth);
+            BigDecimal averSumN = consumNight.divide(sumMonN, 2, RoundingMode.CEILING);
+            //точность в две цифры после запятой с округлением в большую сторону
+            BigDecimal averageSumN = averSumN.multiply(BigDecimal.valueOf(norm));
+            BigDecimal sumNightNorm = averageSumN.multiply(nightNorm);
+            BigDecimal sumNightMoreNorm = consumNight.subtract(averageSumN).multiply(nightMoreNorm);
+            BigDecimal sumNight = sumNightNorm.add(sumNightMoreNorm);
+
+            BigDecimal result = sumDay.add(sumNight);
+
+            System.out.println("Стоимость по по тарифу 'День' составила: " + sumDay +
+                    "\nСтоимость по тарифу 'Ночь' составила: " + sumNight +
+                    "\nОбщая стоимость потребления за месяц: " + result);
         }
     }
-
-//    public static double sumDay(int consumptionDay, int sumMonth) {
-//        double averageSum = norm * (consumptionDay % sumMonth);
-//        double sumDayNorm = averageSum * dayNorm;
-//        double sumDayMoreNorm = (consumptionDay - averageSum) * dayMoreNorm;
-//        double sum = sumDayNorm + sumDayMoreNorm;
-//        return sum;
-//    }
-
-//    public static double sumNight(int consumptionNight, int sumMonth) {
-//        double averageSum = norm * (consumptionNight % sumMonth);
-//        double sumNightNorm = averageSum * nightNorm;
-//        double sumNightMoreNorm = (consumptionNight - averageSum) * nightMoreNorm;
-//        double sum = sumNightNorm + sumNightMoreNorm;
-//        return sum;
-//    }
-
 }
